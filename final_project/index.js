708a6040092c0139cdb98bbde3474680a9736acc
cosmicbubblegumgirl <simon3m3ll3m@gmail.com>
2026-03-10 23:20:14 +0200

    Add CORS middleware and update package dependencies

--- final_project/index.js
+++ final_project/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session');
+const cors = require('cors');
 
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(
